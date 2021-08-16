e0e5353b8ce2c8477f99da40d69183a977827eeb
Samuel Jefroykin <samueljefroykin@MacBook-Pro-de-Samuel.local>
2021-08-16 17:50:26 +0300

    new requirements strict

--- conversational_sentence_encoder/definitions.py
+++ conversational_sentence_encoder/definitions.py
@@ -1,4 +1,2 @@
-NO_CONTEXT_MODEL_PATH = "https://github.com/davidalami/ConveRT/releases" \
-                        "/download/1.0/nocontext_tf_model.tar.gz "
-MULTI_CONTEXT_MODEL_PATH = "https://github.com/davidalami/ConveRT/releases" \
-                           "/download/1.0/multicontext_tf_model.tar"
+NO_CONTEXT_MODEL_PATH = "https://github.com/davidalami/ConveRT/releases/download/1.0/nocontext_tf_model.tar.gz"
+MULTI_CONTEXT_MODEL_PATH = "https://github.com/davidalami/ConveRT/releases/download/1.0/multicontext_tf_model.tar"
